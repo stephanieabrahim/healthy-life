ab6ba047e29ebe4bf25cc29b13b856a29a641d19
stephanie abrahim <stephanieam46@gmail.com>
2023-11-28 02:22:46 -0300

    pagina healthy

---
+++ enviar_formulario.php
@@ -0,0 +1,37 @@
+<?php
+
+$enviaPara = 'stephanie.abrahim@davinci.edu.ar'; 
+
+$subject = 'Formulario enviado'; 
+
+$enviado="index.html";
+
+
+
+$mensaje = '';
+$primero = true;
+foreach($_POST as $indice => $valor){
+	if(is_array($valor)){
+		$mensaje .= '<strong>'.$indice.': </strong><ul>';
+		foreach($valor as $item){
+			$mensaje .= '<li>'.$item .'</li>';
+		}				
+		$mensaje .= '</ul><br>'; 
+	}else{
+		if($primero){
+			$from = $valor;
+			$primero = false;
+		}
+		$mensaje .= '<strong>'.$indice.': </strong>';
+		$mensaje .= $valor . '<br>';
+		if(strpos($valor, '@')>3 && strpos($valor, '.') > -1){
+			$from = $valor;
+		}
+	}
+}
+$mail_headers  = "MIME-Version: 1.0\r\n";
+$mail_headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+$mail_headers .= 'From: ' . $from . "\r\n";
+@mail($enviaPara, $subject, $mensaje, $mail_headers); 
+header("Location: $enviado");
+?>
